9a2567e7f66437856692483b4b320cc94f094735
Tyler Davis <tdnc85@yahoo.com>
2021-05-22 23:08:58 -0400

    Final Commit for HTML, CSS, and JS

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # JavaScript-Quiz
+
+## Description
+
+## Installation
+
+## Technologies Used
+* JavaScript
+* HTML
+* CSS
+* Git Bash
+* GitHub
+
+## Author
+* Tyler Davis &nbsp; | &nbsp; [GitHub](https://github.com/Twdavis85)
+
+## Review URL
+[GitHub](https://github.com/Twdavis85/JavaScript-Quiz) &nbsp; | &nbsp; 
+[Live](https://twdavis85.github.io/JavaScript-Quiz/)
+
+![The Password Generator application after a password has been generated.](./images/screenshot.png)
--- assets/script.js
+++ assets/script.js
@@ -1,51 +1,49 @@
-var quizEl = document.querySelector(".quiz")
-var timerEl = document.querySelector(".timer")
-var rulesEl = document.querySelector(".rules")
-var startBtn = document.querySelector(".start")
-var questionEl = document.querySelector("#question")
-var gameOverEl = document.querySelector("#gameover")
-var scoreEl = document.querySelector(".score")
-var initials = document.querySelector("#initials")
-var submitBtn = document.querySelector(".submit")
-var scoreList = document.querySelector(".scorelist")
-var scoreCard = document.querySelector("#scorecard")
-var hsInitials = document.querySelector(".hs-initials")
-var hsScore = document.querySelector(".hs-score")
-var resultsEl = document.querySelector("#results")
-var replayBtn = document.querySelector(".back")
+var quizEl = document.querySelector(".quiz");
+var timerEl = document.querySelector(".timer");
+var rulesEl = document.querySelector(".rules");
+var startBtn = document.querySelector(".start");
+var questionEl = document.querySelector("#question");
+var gameOverEl = document.querySelector("#gameover");
+var scoreEl = document.querySelector(".score");
+var initials = document.querySelector("#initials");
+var submitBtn = document.querySelector(".submit");
+var scoreList = document.querySelector(".scorelist");
+var resultsEl = document.querySelector("#results");
+var plusPoints = 10;
+var minusPoints = 5;
+var list = document.querySelector(".list");
 
-
-var questions = [{
-        question: "JavaScript File Has An Extension of:",
-        choices: ["A) .java", "B) .js", "C) .script", "D) .javascript"],
-        correctAnswer: "B) .js"
-    },
-    {
-        question: "Which of the following is not a JavaScript data type:",
-        choices: ["A) .java", "B) .js", "C) .script", "D) .javascript"],
-        correctAnswer: "B) .js"
-    },
-    {
-        question: "JavaScript File Has An Extension of:",
-        choices: ["A) .java", "B) .js", "C) .script", "D) .javascript"],
-        correctAnswer: "B) .js"
-    },
-    {
-        question: "JavaScript File Has An Extension of:",
-        choices: ["A) .java", "B) .js", "C) .script", "D) .javascript"],
-        correctAnswer: "B) .js"
-    },
-    {
-        question: "JavaScript File Has An Extension of:",
-        choices: ["A) .java", "B) .js", "C) .script", "D) .javascript"],
-        correctAnswer: "B) .js"
-    },
-    {
-        question: "JavaScript File Has An Extension of:",
-        choices: ["A) .java", "B) .js", "C) .script", "D) .javascript"],
-        correctAnswer: "B) .js"
-    },
-
+var questions = [
+  {
+    question: "JavaScript File Has An Extension of:",
+    choices: ["A) .java", "B) .js", "C) .script", "D) .javascript"],
+    correctAnswer: "B) .js",
+  },
+  {
+    question: "Which of the following is not a JavaScript data type:",
+    choices: ["A) .java", "B) .js", "C) .script", "D) .javascript"],
+    correctAnswer: "B) .js",
+  },
+  {
+    question: "JavaScript File Has An Extension of:",
+    choices: ["A) .java", "B) .js", "C) .script", "D) .javascript"],
+    correctAnswer: "B) .js",
+  },
+  {
+    question: "JavaScript File Has An Extension of:",
+    choices: ["A) .java", "B) .js", "C) .script", "D) .javascript"],
+    correctAnswer: "B) .js",
+  },
+  {
+    question: "JavaScript File Has An Extension of:",
+    choices: ["A) .java", "B) .js", "C) .script", "D) .javascript"],
+    correctAnswer: "B) .js",
+  },
+  {
+    question: "JavaScript File Has An Extension of:",
+    choices: ["A) .java", "B) .js", "C) .script", "D) .javascript"],
+    correctAnswer: "B) .js",
+  },
 ];
 
 var lastQuestion = questions.length;
@@ -54,136 +52,133 @@ var totalPoints = 0;
 var timeStart = 60;
 var timer;
 var correct;
-
-var penalty = 7
+var penalty = 7;
 
 quizEl.style.display = "none";
 scoreList.style.display = "none";
 
 function startQuiz() {
-    rulesEl.style.display = "none";
-    generateQuiz();
-    timer = setInterval(function () {
-        timeStart--;
-        timerEl.textContent = "Time left: " + timeStart;
-        if (timeStart <= 0) {
-            gameOver();
-            quizEl.style.display = "none";
-        }
-    }, 1000);
+  rulesEl.style.display = "none";
+  generateQuiz();
+  timer = setInterval(function () {
+    timeStart--;
+    timerEl.textContent = "Time left: " + timeStart;
+    if (timeStart <= 0) {
+      gameOver();
+      quizEl.style.display = "none";
+    }
+  }, 1000);
 }
 
 function generateQuiz() {
-    var q = questions[currentQuestion];
-    var choicesDiv = document.querySelector(".choices")
-    choicesDiv.innerHTML = ""
-    questionEl.innerHTML = q.question;
-    q.choices.forEach(function (choice) {
-        var choiceBtn = document.createElement("button");
-        choiceBtn.textContent = choice;
-        choiceBtn.setAttribute("value", choice)
-        choiceBtn.onclick = checkAnswer
-        choicesDiv.appendChild(choiceBtn)
-    })
-    quizEl.style.display = "block";
+  var q = questions[currentQuestion];
+  var choicesDiv = document.querySelector(".choices");
+  choicesDiv.innerHTML = "";
+  questionEl.innerHTML = q.question;
+  q.choices.forEach(function (choice) {
+    var choiceBtn = document.createElement("button");
+    choiceBtn.textContent = choice;
+    choiceBtn.setAttribute("value", choice);
+    choiceBtn.onclick = checkAnswer;
+    choicesDiv.appendChild(choiceBtn);
+  });
+  quizEl.style.display = "block";
 }
 
-
 function showHighScore() {
-
-    rulesEl.style.display = "none";
-    quizEl.style.display = "none";
-    gameOverEl.style.display = "none";
-    scoreList.style.display = "block";
-
+  rulesEl.style.display = "none";
+  quizEl.style.display = "none";
+  gameOverEl.style.display = "none";
+  scoreList.style.display = "block";
+  displayHighscores();
+  setTimeout(disableFunction, 1);
 }
 
 function clearScore() {
-    window.localStorage.clear();
-    hsInitials.textContent = "";
-    hsScore.textContent = "";
+  window.localStorage.clear();
+  list.textContent = "";
 }
 
 function replayQuiz() {
-    scoreList.style.display = "none";
-    gameOverEl.style.display = "none";
-    quizEl.style.display = "none";
-    rulesEl.style.display = "block";
+  scoreList.style.display = "none";
+  gameOverEl.style.display = "none";
+  quizEl.style.display = "none";
+  rulesEl.style.display = "block";
+  return generateQuiz;
 }
 
 function checkAnswer() {
-    console.log(this)
-    correct = questions[currentQuestion].correctAnswer;
-    console.log(correct, currentQuestion);
+  console.log(this);
+  correct = questions[currentQuestion].correctAnswer;
+  console.log(correct, currentQuestion);
+  console.log(totalPoints);
 
-    if (this.value === correct) {
-        totalPoints++;
-        resultsEl.textContent = "Correct";
-        setTimeout(function () {
-            resultsEl.textContent = ""
-        }, 500)
+  if (this.value === correct) {
+    totalPoints = totalPoints + plusPoints;
+    resultsEl.textContent = "✔️";
+    setTimeout(function () {
+      resultsEl.textContent = "";
+    }, 500);
+  } else {
+    resultsEl.textContent = "❌";
+    setTimeout(function () {
+      resultsEl.textContent = "";
+    }, 500);
+    timeStart = timeStart - penalty;
+    totalPoints = totalPoints - minusPoints;
+  }
 
-    } else {
-        resultsEl.textContent = "Wrong";
-        setTimeout(function () {
-            resultsEl.textContent = ""
-        }, 500)
-        timeStart = timeStart - penalty;
-    }
-
-    currentQuestion++;
-    if (currentQuestion === questions.length) {
-        gameOver();
-        quizEl.style.display = "none";
-    } else {
-        generateQuiz();
-    }
+  currentQuestion++;
+  if (currentQuestion === questions.length) {
+    gameOver();
+    quizEl.style.display = "none";
+  } else {
+    generateQuiz();
+  }
 }
 
 function gameOver() {
+  gameOverEl.classList.remove("hide");
+  if (timeStart === 0) {
     timerEl.textContent = "Time's up!";
-    gameOverEl.classList.remove("hide");
-    clearInterval(timer);
-    if (timeStart >= 0) {
-        scoreEl.innerHTML = "";
-        var timeRemaining = timeStart;
-        scoreEl.textContent = "Your final score is: " + timeRemaining;
-        quizEl.style.display = "none";
-    }
-    submitBtn.setAttribute("value", timeRemaining)
-    submitBtn.onclick = highScore
+  } else {
+    timerEl.textContent = "";
+  }
+  clearInterval(timer);
+  if (timeStart >= 0) {
+    scoreEl.innerHTML = "";
+    scoreEl.textContent = "Your final score is: " + totalPoints;
+    quizEl.style.display = "none";
+  }
 }
 
-
+submitBtn.onclick = highScore;
 
 function highScore() {
-    console.log(this.value)
-    var scoreObj = {
-        score: this.value,
-        initials: initials.value.trim().toUpperCase()
-    };
-    console.log(scoreObj)
-    localStorage.setItem("highscoreEntry", JSON.stringify(scoreObj));
-    // gameOverEl.style.display = "none";
-    // scoreList.style.display = "block";
+  var savedHighscores = JSON.parse(localStorage.getItem("savedScores")) || [];
+  var scoreObj = {
+    name: initials.value.trim().toUpperCase(),
+    score: totalPoints,
+  };
+  gameOverEl.style.display = "none";
+  scoreList.style.display = "block";
 
-
-
-
-    var highScores = JSON.parse(localStorage.getItem("highscoreEntry")) || [];
-    for(i = 0; i < highScores.length; i++) {
-        var scoreP = document.createElement('ul');
-        var scoreLi = document.createElement('li');
-        scoreLi.textContent = highScores[i].initials, highScores[i].score;
-        scoreP.append("li");
-    //     scoreP.textContent = [highScores.initials + "-" + highScores.score];
-    //     hsInitials.appendChild(scoreP);
-
-    }
-    
-    // hsInitials.innerHTML = highScores.initials + "-" + highScores.score
-    // hsScore.innerHTML = highScores.score;
+  console.log(scoreObj);
+  savedHighscores.push(scoreObj);
+  localStorage.setItem("savedScores", JSON.stringify(savedHighscores));
+  displayHighscores();
 }
 
-console.log()
+function displayHighscores() {
+  var highscores = JSON.parse(localStorage.getItem("savedScores")) || [];
+
+  for (i = 0; i < highscores.length; i++) {
+    var hsEntry = document.createElement("li");
+    hsEntry.textContent =
+      highscores[i].name + "------------" + highscores[i].score;
+    list.appendChild(hsEntry);
+  }
+}
+
+console.log(initials.value);
 startBtn.addEventListener("click", startQuiz);
